9046918eec9218db50ab4bbfca4ca55e72156cdb
VitalikSay <Vitalij.Saiganov@1pt.com>
2024-02-09 21:59:31 +0300

    SLL frenzy

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ start_time = time.time()
 from Source.CF_replace_symbols import CF_Reel, CF_Reelset, Insert_CF_Symbols_instead_of_11
 
 ###################################### НАДО ВОТ ЭТО ЗАДАТЬ ######################################
-GAME_NAME = "SFS_vit"
-INNER_DIRECTORY = ""   # Имя папки внутри основной папки GAME_NAME, если таковой нет, то просто оставляй пустую строчку ""
-SETTING_FILE_NAME = "blocker_even"             # Только имя файла в папке Settings, путь и расширение не надо
+GAME_NAME = "SLL"
+INNER_DIRECTORY = "Frenzy"   # Имя папки внутри основной папки GAME_NAME, если таковой нет, то просто оставляй пустую строчку ""
+SETTING_FILE_NAME = "frenzy_1_reel"             # Только имя файла в папке Settings, путь и расширение не надо
 REELS_FILE_NAME = ""                       # Только имя файла в папке Reels, путь и расширение не надо
 #################################################################################################
 
@@ -18,11 +18,6 @@ reelset = Reelset(reel_data)
 #reelset = CF_Reelset(reel_data)
 reelset.MakeReelSet()
 
-for reel in reelset.reels:
-    for i in range(len(reel.symbols)):
-        window = reel.TakeWindow(i,3)
-        if 8 in window:
-            reel.weights[i] = 0
 
 
 #Insert_CF_Symbols_instead_of_11(reelset)
